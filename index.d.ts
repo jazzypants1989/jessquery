94b0a039a52806cf40625b81d64aac415d2e8a1b
Jesse Pence <jessepence@gmail.com>
2023-10-03 14:37:53 -0700

    lol

---
+++ index.d.ts
@@ -0,0 +1,224 @@
+/**
+ * An HTML element with some extra methods
+ */
+interface DomElement {
+  /** Add an event listener to the element
+   * @param ev The event name
+   * @param fn The event listener
+   * @returns This DomElement
+   * @example
+   * $('button').on('click', () => console.log('clicked'))
+   */
+  on: (ev: string, fn: EventListenerOrEventListenerObject) => DomElement
+  /** Add a CSS Rule to the element
+   * @param prop The CSS property
+   * @param value The CSS value
+   * @returns This DomElement
+   * @example
+   * $('button').css('color', 'red')
+   */
+  css: (prop: string, value: string) => DomElement
+  /** Add a class to the element
+   * @param className The class name
+   * @returns This DomElement
+   * @example
+   * $('button').addClass('btn')
+   */
+  addClass: (className: string) => DomElement
+  /** Remove a class from the element
+   * @param className The class name
+   * @returns This DomElement
+   * @example
+   * $('button').removeClass('btn')
+   */
+  removeClass: (className: string) => DomElement
+  /** Toggle a class on the element
+   * @param className The class name
+   * @returns This DomElement
+   * @example
+   * $('button').toggleClass('btn')
+   */
+  toggleClass: (className: string) => DomElement
+  /** Set an attribute on the element
+   * @param attr The attribute name
+   * @param value The attribute value
+   * @returns This DomElement
+   */
+  setAttribute: (attr: string, value: string) => DomElement
+  /** Append an additional element to the element
+   * @param htmlString The HTML string to append
+   * @returns This DomElement
+   * @example
+   * $('button').append('<span>Click me!</span>')
+   */
+  append: (htmlString: string) => DomElement
+  /** Remove the element from the DOM
+   * @returns This DomElement
+   * @example
+   * $('button').remove()
+   */
+  remove: () => DomElement
+  /** Change the HTML of the element
+   * @param newHtml The new HTML
+   * @returns This DomElement
+   * @example
+   * $('button').html('<span>Click me!</span>')
+   */
+  html: (newHtml: string) => DomElement
+  /** Change the text of the element
+   * @param newText The new text
+   * @returns This DomElement
+   * @example
+   * $('button').text('Click me!')
+   */
+  text: (newText: string) => DomElement
+  /** Animate the element using the WAAPI
+   * @param keyframes The keyframes to animate
+   * @param options The animation options
+   * @returns This DomElement
+   * @example
+   * $('button').animate([{ opacity: 0 }, { opacity: 1 }], { duration: 1000 })
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
+   */
+  animate: (
+    keyframes: Keyframe[] | PropertyIndexedKeyframes,
+    options: KeyframeAnimationOptions
+  ) => DomElement
+}
+/**
+ * An HTML element collection with some extra methods
+ */
+interface DomElementCollection {
+  /** Add an event listener to the elements
+   * @param ev The event name
+   * @param fn The event listener
+   * @returns This DomElementCollection
+   * @example
+   * $('button').on('click', () => console.log('clicked'))
+   */
+  on(ev: string, fn: EventListenerOrEventListenerObject): DomElementCollection
+
+  /** Add a class to the elements
+   * @param className The class name
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').addClass('btn')
+   */
+  addClass: (className: string) => DomElementCollection
+
+  /** Remove a class from the elements
+   * @param className The class name
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').removeClass('btn')
+   */
+  removeClass: (className: string) => DomElementCollection
+
+  /** Toggle a class on the elements
+   * @param className The class name
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').toggleClass('btn')
+   */
+  toggleClass: (className: string) => DomElementCollection
+
+  /** Set an attribute on the elements
+   * @param attr The attribute name
+   * @param value The attribute value
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').setAttribute('disabled', 'true')
+   */
+  setAttribute: (attr: string, value: string) => DomElementCollection
+
+  /** Append an additional element to the elements
+   * @param htmlString The HTML string to append
+   * @returns This DomElementCollection
+   * @example
+   * $$('.container').append('<span>New Element</span>')
+   */
+  append: (htmlString: string) => DomElementCollection
+
+  /** Remove the elements from the DOM
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').remove()
+   */
+  remove: () => DomElementCollection
+
+  /** Append the elements to a target
+   * @param target The target selector
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').appendTo('.container')
+   */
+  appendTo: (target: string) => DomElementCollection
+
+  /** Find descendants matching a sub-selector
+   * @param subSelector The sub-selector
+   * @returns This DomElementCollection
+   * @example
+   * $$('.container').find('.buttons')
+   */
+  find: (subSelector: string) => DomElementCollection
+
+  /** Get the closest ancestor matching a selector
+   * @param ancestorSelector The ancestor selector
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').closest('.container')
+   */
+  closest: (ancestorSelector: string) => DomElementCollection
+
+  /** Delegate an event listener to the elements
+   * @param event The event name
+   * @param subSelector The sub-selector
+   * @param handler The event handler
+   * @returns This DomElementCollection
+   * @example
+   * $$('.container').delegate('click', '.buttons', (e) => console.log('Button clicked'))
+   */
+  delegate: (
+    event: string,
+    subSelector: string,
+    handler: EventListenerOrEventListenerObject
+  ) => DomElementCollection
+
+  /** Animate the elements using the WAAPI
+   * @param keyframes The keyframes to animate
+   * @param options The animation options
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').animate([{ opacity: 0 }, { opacity: 1 }], { duration: 1000 })
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
+   */
+  animate: (
+    keyframes: Keyframe[] | PropertyIndexedKeyframes,
+    options: KeyframeAnimationOptions
+  ) => DomElementCollection
+
+  /** Add a CSS Rule to the elements
+   * @param prop The CSS property
+   * @param value The CSS value
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').css('color', 'red')
+   */
+  css: (prop: string, value: string) => DomElementCollection
+
+  /** Change the HTML of the elements
+   * @param newHtml The new HTML
+   * @returns This DomElementCollection
+   * @example
+   * $$('.container').html('<span>New Content</span>')
+   */
+  html: (newHtml: string) => DomElementCollection
+
+  /** Change the text of the elements
+   * @param newText The new text
+   * @returns This DomElementCollection
+   * @example
+   * $$('.buttons').text('Click me!')
+   */
+  text: (newText: string) => DomElementCollection
+}
---
+++ index.js
@@ -0,0 +1,175 @@
+/** Finds the first element in the DOM that matches a CSS selector and returns it with some extra, useful methods.
+ * @param {string} selector - The CSS selector to match
+ * @returns {DomElement}
+ * @example
+ * $('button').on('click', () => console.log('Clicked!'))
+ * // The first button on the page will log 'Clicked!' when clicked
+ */
+function $(selector) {
+  /** @type {HTMLElement | null} */
+  const element = document.querySelector(selector)
+  if (!element) throw new Error(selector + " is not found on the page")
+  /** @type {DomElement} */
+  const self = {
+    on: (ev, fn) => {
+      element.addEventListener(ev, fn)
+      return self
+    },
+    css: (prop, value) => {
+      element.style[prop] = value
+      return self
+    },
+    addClass: (className) => {
+      element.classList.add(className)
+      return self
+    },
+    removeClass: (className) => {
+      element.classList.remove(className)
+      return self
+    },
+    toggleClass: (className) => {
+      element.classList.toggle(className)
+      return self
+    },
+    setAttribute: (attr, value) => {
+      element.setAttribute(attr, value)
+      return self
+    },
+    append: (htmlString) => {
+      element.insertAdjacentHTML("beforeend", htmlString)
+      return self
+    },
+    remove: () => {
+      element.remove()
+      return self
+    },
+    html: (newHtml) => {
+      element.innerHTML = newHtml
+      return self
+    },
+    text: (newText) => {
+      element.textContent = newText
+      return self
+    },
+    animate: (keyframes, options) => {
+      const animation = element.animate(keyframes, options)
+      animation.onfinish = () => self // Return self on animation finish for chaining
+      return self
+    },
+  }
+
+  return self
+}
+
+/**
+ * Finds all elements in the DOM that match a CSS selector and returns them with some extra, useful methods.
+ * @param {string} selector - The CSS selector to match
+ * @returns {DomElementCollection}
+ * @example
+ * $$('.button').on('click', () => console.log('Clicked!'))
+ * // Every button on the page will log 'Clicked!' when clicked
+ */
+function $$(selector) {
+  /** @type {HTMLElement[]} */
+  const elements = Array.from(document.querySelectorAll(selector))
+  const noop = () => self
+
+  const applyToElements =
+    (method) =>
+    (...args) => {
+      elements.forEach((element) => {
+        const [namespace, func] = method.split(".")
+        const context = namespace ? element[namespace] : element
+        context[func](...args)
+      })
+      return self
+    }
+
+  const checkAndApply = (method) =>
+    elements.length ? applyToElements(method) : noop
+
+  const applyFunc =
+    (customFunction) =>
+    (...args) => {
+      customFunction(...args)
+      return self
+    }
+
+  /** @type {DomElementCollection} */
+  const self = {
+    on: (ev, fn) =>
+      applyFunc((ev, fn) => {
+        elements.forEach((element) => element.addEventListener(ev, fn))
+      })(ev, fn),
+    addClass: checkAndApply("classList.add"),
+    removeClass: checkAndApply("classList.remove"),
+    toggleClass: checkAndApply("classList.toggle"),
+    setAttribute: checkAndApply("setAttribute"),
+    append: applyFunc((htmlString) => {
+      elements.forEach((element) =>
+        element.insertAdjacentHTML("beforeend", htmlString)
+      )
+    }),
+    remove: applyFunc(() => {
+      elements.forEach((element) => element.remove())
+    }),
+    appendTo: applyFunc((target) => {
+      const targetElement = document.querySelector(target)
+      elements.forEach((element) => targetElement.appendChild(element))
+    }),
+    find: (subSelector) => $$(selector + " " + subSelector),
+    closest: (ancestorSelector) => {
+      const ancestors = elements.map((element) =>
+        element.closest(ancestorSelector)
+      )
+      return $$(ancestors.map((ancestor) => ancestor?.tagName).join(","))
+    },
+    delegate: applyFunc((event, subSelector, handler) => {
+      elements.forEach((element) => {
+        element.addEventListener(event, (e) => {
+          if (e.target.matches(subSelector)) handler.call(e.target, e)
+        })
+      })
+    }),
+    animate: applyFunc((keyframes, options) => {
+      elements.forEach((element) => {
+        const animation = element.animate(keyframes, options)
+        animation.onfinish = () => self // Return self on animation finish for chaining
+      })
+    }),
+    css: applyFunc((prop, value) => {
+      elements.forEach((element) => (element.style[prop] = value))
+    }),
+    html: applyFunc((newHtml) => {
+      elements.forEach((element) => (element.innerHTML = newHtml))
+    }),
+    text: applyFunc((newText) => {
+      elements.forEach((element) => (element.textContent = newText))
+    }),
+  }
+
+  const handler = {
+    get: (target, property) => {
+      if (property in self) {
+        return typeof self[property] === "function"
+          ? self[property].bind(self)
+          : self[property]
+      } else {
+        return elements[property]
+      }
+    },
+    set: (target, property, value) => {
+      elements[property] = value
+      return true
+    },
+    apply: (target, thisArg, argumentsList) => {
+      return target(...argumentsList)
+    },
+  }
+
+  const proxy = new Proxy(self, handler)
+
+  return proxy
+}
+
+export { $, $$ }
